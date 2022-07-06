e694de89572f1cf0c64bfc104d634aecc7b2f8d4
Gratitude101 <agabaiduandrew@gmail.com>
2022-07-06 17:23:53 +0100

    login script

---
+++ cypress/integration/Authentication.js
@@ -0,0 +1,19 @@
+/// <reference types = "cypress" />
+
+describe("Authentication page", () =>{ 
+it("verify user is directed to the website" ,() =>{
+cy.visit("https://selfcare.spectranet.com.ng/")
+
+
+
+//
+        
+
+
+            
+   
+    }) 
+
+
+  
+  })
